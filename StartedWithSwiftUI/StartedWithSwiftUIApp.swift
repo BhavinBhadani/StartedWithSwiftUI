f2e7a07b4ab8db732a922f9656bd7f417960023f
bhavinbhadani <bhavinbhadani@yahoo.in>
2022-08-17 10:02:44 +0530

    Initial Commit

---
+++ StartedWithSwiftUI/StartedWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartedWithSwiftUIApp.swift
+//  StartedWithSwiftUI
+//
+//  Created by Bhavin Bhadani on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct StartedWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
