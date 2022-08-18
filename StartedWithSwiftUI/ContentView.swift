4d003a99e65849e42b97f3b846a83be992d60691
bhavinbhadani <bhavinbhadani@yahoo.in>
2022-08-18 16:42:43 +0530

    shape intro

--- StartedWithSwiftUI/ContentView.swift
+++ StartedWithSwiftUI/ContentView.swift
@@ -14,8 +14,43 @@ struct ContentView: View {
     }
 }
 
+struct ShapeViews: View {
+    var body: some View {
+        VStack {
+            Capsule(style: .circular)
+                .frame(width: 200, height: 40)
+                .foregroundColor(.blue)
+            
+            Capsule(style: .continuous)
+                .frame(width: 100, height: 50)
+                .foregroundColor(.red)
+
+            Circle()
+                .frame(width: 30, height: 30)
+            
+            Ellipse()
+                .frame(width: 80, height: 30)
+            
+            Rectangle()
+                .frame(width: 80, height: 30)
+
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: 80, height: 30)
+            
+            Image("zen")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+            
+
+        }
+        .padding()
+    }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ShapeViews()
     }
 }
